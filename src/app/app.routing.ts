a45f07542f30e9041655d9b186494c165428e752
Henrique Klayton <Henrique70031@users.noreply.github.com>
2021-07-04 21:57:45 -0300

    Configurar rota para o layout

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: LayoutComponent,
     children: []
   }
 ];
