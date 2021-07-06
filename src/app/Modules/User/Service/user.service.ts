032bc93eb618d89dec4ade1a1ded48828fda0d63
Henrique Klayton <henrique70031@gmail.com>
2021-07-06 17:18:07 -0300

    Serviços para pegar e criar documentos

--- src/app/Modules/User/Service/user.service.ts
+++ src/app/Modules/User/Service/user.service.ts
@@ -8,15 +8,31 @@ import { UserModel } from '../Model/user.model';
 export class UserService {
     constructor(private _firestore: AngularFirestore) { }
 
+    // public fetchData(): Observable<UserModel[]> {
+    //     console.log("fetch");
+    //     return this._firestore.collection<UserModel>("Users").snapshotChanges().pipe(
+    //         map(actions => actions.map(u => {
+    //             // console.log(actions);
+    //             const data = u.payload.doc.data();
+    //             // console.log("teste", data);
+    //             const id = u.payload.doc.id;
+    //             return new UserModel({id, ...data});
+    //         }))
+    //     );
+    // }
+
     public fetchData(): Observable<UserModel[]> {
-        return this._firestore.collection("Users").snapshotChanges().pipe(
-            map((u: any) => {
-                return [];
-            })
-        );
+        console.log("test");
+        return this._firestore.collection<UserModel>("Users").valueChanges({ idField: "id"});
     }
 
     public insertUser(obj: UserModel): Promise<void> {
         return this._firestore.collection("Users").doc().set(obj);
     }
+
+    public deleteUser(id: string) {
+        return this._firestore.collection("Users").doc(id).delete()
+    }
+
+    updateUser(id: string) {}
 }
