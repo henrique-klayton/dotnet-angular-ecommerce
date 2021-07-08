a440918c9e93e672b3a80ec48569159952a99479
Henrique Klayton <henrique70031@gmail.com>
2021-07-08 14:21:48 -0300

    Serviço CRUD de endereços

--- src/app/Modules/endereco/service/endereco.service.ts
+++ src/app/Modules/endereco/service/endereco.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { AddressModel } from "../model/address.model";
 
 @Injectable()
-export class EnderecoService {}
+export class EnderecoService {
+    constructor(private _firestore: AngularFirestore) {}
+
+    public fetchData(): Observable<AddressModel[]> {
+        return this._firestore.collection<AddressModel>("Enderecos").valueChanges();
+    }
+    
+    public fetchAddressByCep(cep: string): Observable<AddressModel> {
+        return this._firestore.collection<AddressModel>("Enderecos").doc(cep).get().pipe(
+            map(u => new AddressModel(u.data()))
+        );
+    }
+
+    public insertOrUpdateAddress(obj: AddressModel): Promise<void> {
+        return this._firestore.collection("Endereco").doc(obj.cep).set(obj);
+    }
+
+    public deleteAddress(cep: string): Promise<void> {
+        return this._firestore.collection("Endereco").doc(cep).delete();
+    }
+}
