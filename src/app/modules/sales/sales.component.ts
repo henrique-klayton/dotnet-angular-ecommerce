f827dcd4393f1e2d814e3e64957e2899f34f7e6b
Henrique Klayton <Henrique70031@users.noreply.github.com>
2021-07-19 23:32:56 -0300

    Tela de vendas

--- src/app/modules/sales/sales.component.ts
+++ src/app/modules/sales/sales.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ProductFormModel } from '../product/form/model/product.form.model';
+import { ProductService } from '../product/service/product.service';
 
 @Component({
-  selector: 'app-sales',
-  templateUrl: './sales.component.html',
-  styleUrls: ['./sales.component.scss']
+	selector: 'app-sales',
+	templateUrl: './sales.component.html',
+	styleUrls: ['./sales.component.scss']
 })
-export class SalesComponent implements OnInit {
+export class SalesComponent implements OnInit, OnDestroy {
+	public products: ProductFormModel[];
 
-  constructor() { }
+	private _onDestroy = new Subject<void>();
 
-  ngOnInit(): void {
-  }
+	constructor(private _productService: ProductService) { }
 
+	ngOnInit(): void {
+		this._productService.fetchData()
+			.pipe(takeUntil(this._onDestroy))
+			.subscribe((res) => this.products = res);
+	}
+
+	ngOnDestroy(): void {
+		this._onDestroy.next();
+		this._onDestroy.complete();
+	}
 }
--- src/app/modules/sales/sales.module.ts
+++ src/app/modules/sales/sales.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { SalesRoutingModule } from './sales-routing.module';
 import { SalesComponent } from './sales.component';
 import { MaterialModule } from 'src/app/material.module';
+import { ProductService } from '../product/service/product.service';
 
 
 @NgModule({
-  declarations: [
-    SalesComponent
-  ],
-  imports: [
-    CommonModule,
-    SalesRoutingModule,
-    MaterialModule
-  ]
+	declarations: [
+		SalesComponent
+	],
+	imports: [
+		CommonModule,
+		SalesRoutingModule,
+		MaterialModule
+	],
+	providers: [
+		ProductService
+	]
 })
 export class SalesModule { }
