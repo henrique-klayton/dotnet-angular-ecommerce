5fd4dda641032bc725dd890cbb959c4de04f9d25
Henrique Klayton <Henrique70031@users.noreply.github.com>
2021-07-29 13:30:31 -0300

    Deixar métodos do BaseService protected
    Para não aparecerem em outros serviços

--- src/app/shared/service/base.service.ts
+++ src/app/shared/service/base.service.ts
@@ -8,27 +8,19 @@ export interface IGetOptions {
 	idField?: string
 }
 
-interface IBaseService {
-	getData<T>(col: string, options?: IGetOptions): Observable<T[]>;
-	getById<T>(id: string, col: string, options?: IGetOptions): Observable<T>;
-	create<T>(obj: T, cls: Constructable<T>, col: string): Promise<void>;
-	update<T>(id: string, obj: T, cls: Constructable<T>, col: string): Promise<void>;
-	delete(id: string, collection: string): Promise<void>;
-}
-
-export class BaseService implements IBaseService {
+export class BaseService {
 	protected _firestore: AngularFirestore;
 
 	constructor() {
 		this._firestore = AppInjector.injector.get(AngularFirestore);
 	}
-	getData<T>(
+	protected getData<T>(
 		col: string,
 		options: IGetOptions = { idField: 'id' }
 	): Observable<T[]> {
 		return this._firestore.collection<T>(col).valueChanges({ idField: options.idField });
 	}
-	getById<T>(
+	protected getById<T>(
 		id: string,
 		col: string,
 		options: IGetOptions = { idField: 'id' }
@@ -46,10 +38,10 @@ export class BaseService implements IBaseService {
 				})
 			);
 	}
-	create = <T>(obj: T, cls: Constructable<T>, col: string, id?: string): Promise<void> =>
+	protected create = <T>(obj: T, cls: Constructable<T>, col: string, id?: string): Promise<void> =>
 		this._firestore.collection(col).doc(id).set(formatObjectToFirebase(obj, cls));
-	update = <T>(id: string, obj: T, cls: Constructable<T>, col: string): Promise<void> =>
+	protected update = <T>(id: string, obj: T, cls: Constructable<T>, col: string): Promise<void> =>
 		this._firestore.collection(col).doc(id).set(formatObjectToFirebase(obj, cls));
-	delete = (id: string, col: string): Promise<void> =>
+	protected delete = (id: string, col: string): Promise<void> =>
 		this._firestore.collection(col).doc(id).delete();
 }
