94f4e573c5558d88600d928bd82b8839d640f8d5
Henrique Klayton <Henrique70031@users.noreply.github.com>
2021-08-23 12:10:35 -0300

    Criar contexto de conexão com banco de dados

---
+++ backend/EcommerceContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace E_commerce.Models {
+	public partial class EcommerceContext : DbContext {
+		public EcommerceContext() {
+		}
+
+		public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options) {
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			if (!optionsBuilder.IsConfigured) {
+				optionsBuilder
+					.UseMySql("name=DatabaseConnectionString", ServerVersion.Parse("10.6.4-mariadb"));
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			modelBuilder.HasCharSet("utf8mb4")
+					.UseCollation("utf8mb4_general_ci");
+
+			OnModelCreatingPartial(modelBuilder);
+		}
+
+		partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+	}
+}
