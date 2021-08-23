c321066447d9424d7c6a8fa95194e821a35d5b90
Henrique Klayton <Henrique70031@users.noreply.github.com>
2021-08-23 15:47:00 -0300

    fix: Não utilizar o construtor que foi removido

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -23,11 +23,11 @@ namespace E_commerce.Controllers {
 		[HttpGet]
 		public IEnumerable<WeatherForecast> Get() {
 			var rng = new Random();
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast(
-				DateTime.Now.AddDays(index),
-				rng.Next(-20, 55),
-				Summaries[rng.Next(Summaries.Length)]
-			)).ToArray();
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			}).ToArray();
 		}
 	}
 }
