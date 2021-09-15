6435c2d046dafb4f13433f01e238c8772bfb414f
Henrique Klayton <Henrique70031@users.noreply.github.com>
2021-09-15 15:33:19 -0300

    Método PATCH para alterar categoria de um produto

--- backend/Controllers/ProductController.cs
+++ backend/Controllers/ProductController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Ecommerce.Models;
+using Ecommerce.Utils;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -42,6 +44,34 @@ namespace Ecommerce.Controllers {
 
 		// 	return StatusCode(201);
 		// }
+
+		// [HttpPatch("{id:int}")]
+		// public IActionResult Patch(int id, JsonPatchDocument<>) {
+		// 	// Console.WriteLine(model.Name);
+		// 	// _dbContext.Products.Add(model);
+		// 	// _dbContext.SaveChanges();
+
+		// 	return StatusCode(201);
+		// }
+
+		[HttpPatch("{id:int}/[action]/{categoryId:int}")]
+		public IActionResult Category(int id, int categoryId) {
+			var category = _dbContext.Categories.SingleOrDefault(c => c.Id == categoryId);
+			if (category == null) return EntityNotFound(categoryId, "Categoria", Gender.F);
+
+			var product = _dbContext.Products
+				.Include(p => p.Category)
+				.SingleOrDefault(p => p.Id == id);
+			if (product == null) return EntityNotFound(id);
+
+			product.CategoryId = categoryId;
+			product.Category = category;
+
+			_dbContext.Products.Update(product);
+			_dbContext.SaveChanges();
+
+			return Ok();
+		}
 
 		[HttpDelete("{id}")]
 		public IActionResult Delete(int id) {
