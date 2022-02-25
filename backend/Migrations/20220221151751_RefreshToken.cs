cb8c0f93f5d19217bbc3116d9fbeb9f75e1f53ff
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2022-02-25 15:15:01 -0300

    Adicionar migração do refresh token

---
+++ backend/Migrations/20220221151751_RefreshToken.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Migrations
+{
+    public partial class RefreshToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                collation: "utf8mb4_general_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+        }
+    }
+}
